36dcbaa0993fc139062d2731b7defcea906e2d52
Tong <1@3.com>
2020-02-03 20:39:03 -0800

    clear irrelevant directories and files.

--- Typecheck.java
+++ Typecheck.java
@@ -6,14 +6,14 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
-public class Main {
+public class Typecheck {
 	public static void main(String[] args) {
 		try {
 			InputStream in = new FileInputStream(args[1]);
 			Node goal = new MiniJavaParser(in).Goal();
 
 			goal.accept(new MyVisitor());
-			System.out.println("successfully");
+			System.out.println("Program type checked successfully");
 		} catch (ParseException | FileNotFoundException e) {
 			e.printStackTrace();
 		}
