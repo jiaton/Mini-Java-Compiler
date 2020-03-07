0b33e34132512c5c04d898e6173a6105f6140f28
Lycheeicy <49021065+Lycheeicy@users.noreply.github.com>
2020-03-07 15:00:30 -0800

    fix bug in getoutsize

--- RegisterAllocation/V2VM.java
+++ RegisterAllocation/V2VM.java
@@ -67,13 +67,18 @@ public class V2VM {
 
     public static int getOutSize(String ident){
         int paras = 0;
-        for(String fstr : callerMap.get(ident)){
-            for(VFunction function : tree.functions) {
-                if (function.ident.equals(fstr)) {
-                    if(paras<function.params.length-3)
-                        paras = function.params.length-3;
+        HashSet<String> s;
+        s=callerMap.get(ident);
+        if(s == null){
+            return 0;
+        }
+        for(String fstr : s){
+                for(VFunction function : tree.functions) {
+                    if (function.ident.equals(fstr)) {
+                        if(paras<function.params.length-3)
+                            paras = function.params.length-3;
+                    }
                 }
-            }
         }
         if(paras < 0)   paras = 0;
 
